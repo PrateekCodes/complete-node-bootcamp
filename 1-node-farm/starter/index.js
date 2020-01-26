cb209ca4145751c7b5a613410131ccb6c3208ddd
prateekcodes <prateekisthebest98@gmail.com>
2020-01-26 22:37:52 +0530

    feat: create api route into index.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -35,21 +35,25 @@ const url = require("url");
 //////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === "/" || pathName === "/overview") {
+  if (["/", "/overview"].includes(pathName)) {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html"
     });
     res.end("<h1>Page not found!</h1>");
   }
-
-  res.end("Hello from server!");
 });
 
 server.listen(8000, () => {
